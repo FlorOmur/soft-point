a9dccd28b83e50ff494de5c771bd346bc4fde6f2
Flor Omurbekov <flor.omurbekov@mail.ru>
2022-01-14 23:54:31 +0300

    media

--- js/java.js
+++ js/java.js
@@ -26,3 +26,25 @@ range.addEventListener('input', (e) => {
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
+
+    AOS.init();
+
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 3,
+        loop: true,
+        dots: true,
+        autoplay: true,
+        response: {
+            1200: {
+                items: 4
+            },
+            768: {
+                items: 3,
+            },
+            577:{
+                items: 2
+            },
+        }
+    });
+});
